c39d16e17bc3546d5bed506413172c80eaaf7650
uk1996 <csuk1947@gmail.com>
2022-05-20 13:01:17 +0900

    유저 페이지 Follow, UnFollow 구현

--- frontend/src/pages/accounts/Profile.js
+++ frontend/src/pages/accounts/Profile.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import AppLayout from '../../components/AppLayout';
 import Axios from 'axios';
 import { useUrlContext } from '../../utils/UrlProvider';
@@ -6,27 +6,40 @@ import { useAppContext } from '../../store';
 import './Profile.scss';
 import UserAvatar from '../../components/UserAvatar';
 import { usePostListContext, setPostList } from '../../utils/PostListProvider';
-import { Row, Col } from 'antd';
+import { Row, Col, Button } from 'antd';
+import { useMyUserContext } from '../../utils/MyUserProvider';
 
 const Profile = ({ location }) => {
     const defaultUrl = useUrlContext().defaulturl;
-    const apiUrl = defaultUrl + '/api/user_page/';
+    const userPageUrl = defaultUrl + '/api/user_page/';
+    const followUrl = defaultUrl + '/accounts/follow/';
+    const unFollowUrl = defaultUrl + '/accounts/unfollow/';
     const {
         store: { jwtToken },
     } = useAppContext();
     const [username, setUserName] = useState();
     const [user, setUser] = useState();
     const { postList, dispatch } = usePostListContext();
+    const [isFollow, setIsFollow] = useState();
+    const { myUser } = useMyUserContext();
 
     useEffect(() => {
         setUserName(location.pathname.split('/').at(-1));
     }, [location]);
 
     useEffect(() => {
-        const headers = { Authorization: `Bearer ${jwtToken}` };
+        if (myUser) {
+            const myFollowingUser = myUser.following_set.map(
+                (user) => user.username,
+            );
+            setIsFollow(myFollowingUser.includes(username));
+        }
+    }, [myUser, username]);
 
+    useEffect(() => {
+        const headers = { Authorization: `Bearer ${jwtToken}` };
         if (username) {
-            Axios.post(apiUrl, { username }, { headers })
+            Axios.post(userPageUrl, { username }, { headers })
                 .then((response) => {
                     const { user, postList } = response.data;
                     setUser(user);
@@ -36,7 +49,42 @@ const Profile = ({ location }) => {
                     console.log(error);
                 });
         }
-    }, [username, apiUrl, jwtToken, dispatch]);
+    }, [username, userPageUrl, jwtToken, dispatch]);
+
+    const followClick = useCallback(() => {
+        if (username) {
+            setIsFollow(true);
+            const headers = { Authorization: `Bearer ${jwtToken}` };
+            Axios.post(followUrl, { username }, { headers }).then(() => {
+                Axios.post(userPageUrl, { username }, { headers })
+                    .then((response) => {
+                        const { user, postList } = response.data;
+                        setUser(user);
+                        dispatch(setPostList(postList));
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            });
+        }
+    }, [jwtToken, username, followUrl, dispatch, userPageUrl]);
+    const unFollowClick = useCallback(() => {
+        if (username) {
+            setIsFollow(false);
+            const headers = { Authorization: `Bearer ${jwtToken}` };
+            Axios.post(unFollowUrl, { username }, { headers }).then(() => {
+                Axios.post(userPageUrl, { username }, { headers })
+                    .then((response) => {
+                        const { user, postList } = response.data;
+                        setUser(user);
+                        dispatch(setPostList(postList));
+                    })
+                    .catch((error) => {
+                        console.log(error);
+                    });
+            });
+        }
+    }, [jwtToken, username, unFollowUrl, dispatch, userPageUrl]);
 
     return (
         <AppLayout contentwidth="100%">
@@ -64,6 +112,20 @@ const Profile = ({ location }) => {
                                     팔로잉 {user.following_set.length}
                                 </div>
                             </div>
+                            {myUser.username !== username && (
+                                <div>
+                                    {isFollow && (
+                                        <Button onClick={unFollowClick}>
+                                            UnFollow
+                                        </Button>
+                                    )}
+                                    {!isFollow && (
+                                        <Button onClick={followClick}>
+                                            Follow
+                                        </Button>
+                                    )}
+                                </div>
+                            )}
                         </div>
                     </div>
                     <div className="postList">
