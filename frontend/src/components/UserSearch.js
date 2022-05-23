b5e94c1cbc7d0e3c4f17eedcbd65c2715fff9f56
SW Choi <csuk1947@gmail.com>
2022-05-23 19:26:07 +0900

    userSearch 기능 수정

--- frontend/src/components/UserSearch.js
+++ frontend/src/components/UserSearch.js
@@ -1,18 +1,19 @@
 import React, { useEffect, useState } from 'react';
 import { Input, AutoComplete, Avatar, Row, Col } from 'antd';
-import Axios from 'axios';
 import { useUrlContext } from '../utils/UrlProvider';
 import { useAppContext } from '../store';
 import { useHistory } from 'react-router-dom';
+import useAxios from 'axios-hooks';
 
 const UserSearch = () => {
     const [userName, setUserName] = useState('');
     const defaultUrl = useUrlContext().defaulturl;
     const apiUrl = defaultUrl + '/accounts/usersearch/';
-    const [userList, setUserList] = useState([]);
+    const requestUrl = apiUrl + `?username=${userName}`;
     const {
         store: { jwtToken },
     } = useAppContext();
+    const headers = { Authorization: `Bearer ${jwtToken}` };
     const history = useHistory();
     const [options, setOptions] = useState([]);
 
@@ -20,41 +21,39 @@ const UserSearch = () => {
         setUserName(target.value);
     };
 
-    useEffect(() => {
-        const headers = { Authorization: `Bearer ${jwtToken}` };
-        const requestUrl = apiUrl + `?username=${userName}`;
-        Axios.get(requestUrl, { headers }).then((response) => {
-            setUserList(response.data);
-            if (userName.length === 0) {
-                setUserList([]);
-            }
-        });
-    }, [userName, jwtToken, apiUrl]);
+    const [{ data: userList }] = useAxios({
+        url: requestUrl,
+        headers,
+    });
 
     useEffect(() => {
         setOptions(
-            userList.map((user) => {
-                return {
-                    key: user.pk,
-                    value: (
-                        <Row>
-                            <Col span={8}>
-                                <Avatar
-                                    icon={
-                                        <img
-                                            src={defaultUrl + user.avatar_url}
-                                            alt=""
-                                        />
-                                    }
-                                />
-                            </Col>
-                            <Col span={12}>{user.username}</Col>
-                        </Row>
-                    ),
-                };
-            }),
+            userList &&
+                userName.length > 0 &&
+                userList.map((user) => {
+                    return {
+                        key: user.pk,
+                        value: (
+                            <Row>
+                                <Col span={8}>
+                                    <Avatar
+                                        icon={
+                                            <img
+                                                src={
+                                                    defaultUrl + user.avatar_url
+                                                }
+                                                alt=""
+                                            />
+                                        }
+                                    />
+                                </Col>
+                                <Col span={12}>{user.username}</Col>
+                            </Row>
+                        ),
+                    };
+                }),
         );
-    }, [userList, defaultUrl, history]);
+    }, [userList, defaultUrl, history, userName]);
 
     return (
         <div>
