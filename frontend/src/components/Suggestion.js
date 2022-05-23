d87d32958e439df47cd1cabc9be5c0b5d964e970
SW Choi <csuk1947@gmail.com>
2022-05-23 19:48:55 +0900

    suggestion에서 유저 클릭시 재로딩

--- frontend/src/components/Suggestion.js
+++ frontend/src/components/Suggestion.js
@@ -2,27 +2,35 @@ import React from 'react';
 import { Button, Avatar } from 'antd';
 import './Suggestion.scss';
 import { useUrlContext } from '../utils/UrlProvider';
-import { Link } from 'react-router-dom';
 
 const Suggestion = ({ username, avatar_url, is_follow, onFollowUser }) => {
     const imgUrl = useUrlContext().defaulturl + avatar_url;
 
     return (
         <div className="suggestion">
-            <Link to={'/accounts/profile/' + username}>
-                <div className="avatar">
-                    <Avatar size="small" icon={<img src={imgUrl} alt="" />} />
-                </div>
-            </Link>
-            <Link to={'/accounts/profile/' + username}>
-                <div className="username">
-                    {
-                        <span style={{ color: 'black', opacity: '0.9' }}>
-                            {username}
-                        </span>
-                    }
-                </div>
-            </Link>
+            <div
+                className="avatar"
+                onClick={() => {
+                    window.location.replace(`accounts/profile/${username}`);
+                }}
+                style={{ cursor: 'pointer' }}
+            >
+                <Avatar size="small" icon={<img src={imgUrl} alt="" />} />
+            </div>
+            <div
+                className="username"
+                onClick={() => {
+                    window.location.replace(`accounts/profile/${username}`);
+                }}
+                style={{ cursor: 'pointer' }}
+            >
+                {
+                    <span style={{ color: 'black', opacity: '0.9' }}>
+                        {username}
+                    </span>
+                }
+            </div>
+
             <div className="action">
                 {is_follow && (
                     <span style={{ opacity: '0.5' }}>following...</span>
